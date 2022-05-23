aafb09ba36a27550fc205f4afdb8ac1fd594c8ea
seungmin Ryu <ojysep9987@naver.com>
2022-05-23 11:24:18 +0900

    Django_3week

---
+++ 3주차/유승민/EVERYTIME2/EverytimeClone/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accounts import views as accounts
+
+
+urlpatterns = [
+    path('login/', accounts.login, name='login'),
+    path('register/', accounts.register, name='register')
+]
