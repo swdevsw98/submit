33f072ee7e9ef7d341a8edca1b1b17523a40100b
swdevsw98 <102571918+swdevsw98@users.noreply.github.com>
2022-05-31 20:05:38 +0900

    Delete 4주차 directory

--- 4주차/하성원/2_study/EverytimeClone/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from accounts import views as accounts
-urlpatterns = [
-    path('login/', accounts.login, name='login'),
-    path('register/', accounts.register, name='register'),
-    path('logout/', accounts.logout, name='logout')
-]
